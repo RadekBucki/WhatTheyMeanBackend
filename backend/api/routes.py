865f28634cb1285c2bddc868fd6d9051dc53d8ff
Damiaan <ddamian2057@gmail.com>
2023-10-29 15:23:47 +0100

    file download and exception catcher

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -1,26 +1,33 @@
-from flask import Blueprint, render_template
+import logging
 
-from backend.model.status import Status
+from flask import Blueprint, render_template, request, jsonify
 
 api = Blueprint('api', __name__)
+logger = logging.getLogger(__name__)
 
 @api.route('/register/file', methods=['POST'])
 def register_file():
-    return "Register file"
+    if 'file' not in request.files:
+        raise Exception('No file part')
+
+    file = request.files.get('file')
+    logger.info(f"Received file: {file}")
+
+    return "id and status"
 
 @api.route('/register/url', methods=['POST'])
 def register_url():
-    return "Register url"
+    return "id and status"
 
 @api.route('/analyse/<analyse_id>', methods=['GET'])
 def get_analyse(analyse_id: int):
-    return f"Get analyse {analyse_id} with status {Status.QUEUED}"
+    return f"Get analyse {analyse_id}"
 
 @api.route('/analyse', methods=['GET'])
 def get_analyses():
     return "Get list of analyses"
 
-# for testing purposes
+# for socket connection testing purposes
 @api.route('/', methods=['GET'])
 def for_test():
     return render_template('index.html')
--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ import logging
 from flask import Flask
 from flask_socketio import SocketIO
 
+from backend.api.exception_handlers import handle_500_error, handle_bad_request
 from backend.api.routes import api
 from backend.socket.register_analysis import register_analysis
 from backend.socket.register_socket import register_socketio_events
@@ -16,6 +17,8 @@ logging.basicConfig(level=logging.DEBUG, force=True)
 
 # Register the routes
 app.register_blueprint(api)
+app.register_error_handler(500, handle_500_error)
+app.register_error_handler(Exception, handle_bad_request)
 
 # Register the socketio events
 register_socketio_events(socketio)
