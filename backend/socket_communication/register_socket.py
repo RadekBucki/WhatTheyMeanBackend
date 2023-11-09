1ca603f7b2c2e2a2a34f275fa96cb7f919a21e84
Kamil Bednarek <kamil.bednarek.2001@gmail.com>
2023-11-09 21:51:38 +0100

    Remove test code

--- backend/socket_communication/register_socket.py
+++ backend/socket_communication/register_socket.py
@@ -2,8 +2,6 @@ import logging
 
 from flask_socketio import emit
 
-from backend.video_parser.YouTubeDownloader import YouTubeDownloader
-
 logger = logging.getLogger(__name__)
 
 
@@ -12,7 +10,6 @@ def register_socketio_events(socketio):
     @socketio.on('connect')
     def on_connect():
         logger.info("Connected")
-        YouTubeDownloader.download("https://www.youtube.com/watch?v=SzWv8WD0Sik")
         emit('after connect', "Connected successfully", broadcast=True)
 
     @socketio.on('disconnect')
