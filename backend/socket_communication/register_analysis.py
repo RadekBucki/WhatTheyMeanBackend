a7f6ea9f4e7623b27b276a22991e3eb4daa69d44
Damiaan <ddamian2057@gmail.com>
2023-11-09 20:50:18 +0100

    adding package for exceptions, changing the issue of percentages to regular numbers

--- backend/socket_communication/register_analysis.py
+++ backend/socket_communication/register_analysis.py
@@ -1,4 +1,5 @@
 import logging
+import string
 
 from flask_socketio import emit
 
@@ -8,11 +9,13 @@ logger = logging.getLogger(__name__)
 def register_analysis(socketio):
 
     @socketio.on('analyse')
-    def handle_analyse(analyse_id: int):
-        logger.info(f"Received analyse: {analyse_id}")
+    def handle_analyse(analyse_uuid: string):
+        logger.info(f"Received analyse: {analyse_uuid}")
 
         # file to mp3 start -> completed 10%
         emit('progress', "20", broadcast=True)
 
         # mp3 -> text start -> completed 30%
         emit('progress', "30", broadcast=True)
+
+        emit('completed', analyse_uuid, broadcast=True)
