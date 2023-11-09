292007491652b8934f18d134e5fbb28c320eb1c4
Damiaan <ddamian2057@gmail.com>
2023-11-09 21:24:58 +0100

    Update register_analysis.py

--- backend/socket_communication/register_analysis.py
+++ backend/socket_communication/register_analysis.py
@@ -3,20 +3,29 @@ import string
 
 from flask_socketio import emit
 
+from backend.ai import process_audio, transcribe, sum_up, run_sentiment_analysis
+from backend.video_parser.YouTubeDownloader import YouTubeDownloader
+
 logger = logging.getLogger(__name__)
 
 
 def register_analysis(socketio):
 
+#service
+
     @socketio.on('analyse')
     def handle_analyse(analyse_uuid: string):
         logger.info(f"Received analyse: {analyse_uuid}")
+        analyse = {
+            "url": "https://www.youtube.com/watch?v=NxHrTaWzAL4"
+        }
+        # analyse = service.get_analyse(analyse_uuid)
 
-        # file to mp3 start -> completed 10%
+        base64_file = None
+        if analyse['url']:
+            base64_file = YouTubeDownloader.download(analyse['url'])
         emit('progress', "20", broadcast=True)
 
-        # mp3 -> text start -> completed 30%
-        emit('progress', "30", broadcast=True)
-
-        # text -> sentiment start -> completed 50%
-        emit('progress', "50", broadcast=True)
+        processing_dto = process_audio(base64_file)
+        logger.info(f"Processing result: {processing_dto.sentiment}")
+        emit('progress', "100", broadcast=True)
