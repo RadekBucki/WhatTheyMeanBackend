60c53ebc395f2a5ef6f7a51fc89969317eb07212
Kamil Bednarek <kamil.bednarek.2001@gmail.com>
2023-11-09 21:50:56 +0100

    MP3 youtube

--- backend/socket_communication/register_socket.py
+++ backend/socket_communication/register_socket.py
@@ -2,6 +2,8 @@ import logging
 
 from flask_socketio import emit
 
+from backend.video_parser.YouTubeDownloader import YouTubeDownloader
+
 logger = logging.getLogger(__name__)
 
 
@@ -10,6 +12,7 @@ def register_socketio_events(socketio):
     @socketio.on('connect')
     def on_connect():
         logger.info("Connected")
+        YouTubeDownloader.download("https://www.youtube.com/watch?v=SzWv8WD0Sik")
         emit('after connect', "Connected successfully", broadcast=True)
 
     @socketio.on('disconnect')
